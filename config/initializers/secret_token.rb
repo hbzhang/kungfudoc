84d6983f970e2cb122c9dbed887ebe2822cb7e66
unknown <hbzhang@LTHZANG.ctr.vt.edu>
2013-04-24 22:24:17 -0400

    P

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Kungfudoc::Application.config.secret_token = '81a02ab23893807c1449e28a8aae4ab351861e73ad2efb483d071ee57dc7ac36df96a2dea9f5b68e3af90f8396d5d8db6a4021c1b13f9eaf6aaabee8f2b445d0'
